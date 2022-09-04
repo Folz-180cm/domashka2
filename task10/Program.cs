174871f5f1b6db0d5d9114415e82edcf70b37d27
Ruslan <folzbuisness@gmail.com>
2022-09-04 15:38:14 +0200

    Сделал домашку

---
+++ task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+Console.Write("Введите 3-ёх значное число: ");
+int threeDigit = Convert.ToInt32(Console.ReadLine());
+int final = 0;
+if (threeDigit > 99 && threeDigit < 1000)
+{
+    threeDigit = threeDigit / 10;
+    final = threeDigit % 10;
+    Console.WriteLine($"Вторая цифра числа = {final}");
+}
+else
+{
+    Console.WriteLine($"Ошибка! Число {threeDigit} не 3-ёх значное! Введите другое число.");
+}
---
+++ task15/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int inputnum = Convert.ToInt32(Console.ReadLine());
+if(inputnum>0&&inputnum<8){
+    if(inputnum == 1){
+        Console.WriteLine("Нет");
+    }
+    else if(inputnum == 2){
+        Console.WriteLine("Нет");
+    }
+    else if(inputnum == 3){
+        Console.WriteLine("Нет");
+    }
+    else if(inputnum == 4){
+        Console.WriteLine("Нет");
+    }
+    else if(inputnum == 5){
+        Console.WriteLine("Нет");
+    }
+    else if(inputnum == 6){
+        Console.WriteLine("Да");
+    }
+    else{
+        Console.WriteLine("Да");
+    }
+}
+else{
+    Console.WriteLine("Введите другое число");
+}
